1867f406cf17667066d88fdc9621e52d5ce05873
Ryan Walters <rwalters@eb.com>
2017-05-26 15:31:18 -0500

    build: compile individual components

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,68 @@
 'use strict';
 
 const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const fs = require('fs');
+const glob = require('glob');
 const gulp = require('gulp');
+const path = require('path');
 const sass = require('gulp-sass');
 
-const internals = {
-    distDir: './dist',
-    srcDir: './src',
-    jsFiles: '/**/*.js',
-    sassFiles: '/**/*.scss'
-};
+const DIST_DIR = './dist';
+const SRC_DIR = './src';
+const SASS_GLOB = '/**/*.scss';
 
 
 // --- Tasks
 
-// sass
+// Compile SCSS files
 
 gulp.task('sass', () => {
 
-    return gulp.src(internals.srcDir + internals.sassFiles)
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'iOS >= 6'],
-            cascade: false
-        }))
-        .pipe(gulp.dest(internals.distDir));
+    getFolders(SRC_DIR)
+        .map(folder => {
+            buildCss(path.join(SRC_DIR, folder, '/*.scss'), `${folder}.css`);
+        });
+
+    //buildCss(path.join(SRC_DIR, SASS_GLOB), 'styleguide.css');
 });
 
 
 // Watch
 
 gulp.task('watch', () => {
-    gulp.watch(internals.srcDir + internals.sassFiles, ['sass']);
+    gulp.watch(SRC_DIR + SASS_GLOB, ['sass']);
 });
+
+
+/**
+ * Get a list of folder names
+ *
+ * @param {String} dir          The directory in which to look for folders
+ * @returns {Array<String>}     A list of the folder names within the specified directory
+ */
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+        .filter(file => fs.statSync(path.join(dir, file)).isDirectory());
+}
+
+
+/**
+ * Build the CSS
+ *
+ * @param {String} src          A glob that represents the CSS files to include in the task
+ * @param {String} filename     The name of the file to output
+ * @param {String} [dest]       Destination output directory
+ */
+
+function buildCss(src, filename, dest = DIST_DIR) {
+
+    console.log(src, filename, dest);
+
+    return gulp.src(src)
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(concat(filename))
+        .pipe(autoprefixer({ browsers: ['last 2 versions', 'iOS >= 7'] }))
+        .pipe(gulp.dest(dest));
+}
